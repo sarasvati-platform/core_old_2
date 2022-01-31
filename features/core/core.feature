708516d54fa61fd8b18a08e3573968a93bc48fc4
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-01-31 07:04:02 +0000

    remove unused files

--- features/core/core.feature
+++
@@ -1,6 +0,0 @@
-Feature: Core
-
-  Core feature
-
-  Scenario: Basic scenario
-    Then Hello function should return name 'hello, John' for 'John'
--- tests/features/core/core.feature.ts
+++
@@ -1,9 +0,0 @@
-import { loadFeature, autoBindSteps } from 'jest-cucumber'
-
-import { coreSteps } from '@tests/features/core/steps/core.steps'
-
-autoBindSteps([
-  loadFeature('features/core/core.feature'),
-], [
-  coreSteps,
-])
