b65264dc0c9af9c66afc39fdb31c1317334cbe02
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-01-31 07:03:49 +0000

    docs

--- src/flashcards/models/name.ts
+++ src/flashcards/models/name.ts
@@ -1,4 +1,11 @@
+/**
+ * Name
+ */
 export class Name {
+  /**
+   * Initializes the new instance of the Name class
+   * @param value Name
+   */
   constructor(public readonly value: string) {
     if (value.includes('\n')) {
       throw new Error('The name must not contain new line')
@@ -14,10 +21,19 @@ export class Name {
     }
   }
 
+  /**
+   * Compares the Name to another Name
+   * @param other Other name to compare to
+   * @returns True if the name is equal to the other name
+   */
   public equals(other: Name): boolean {
     return this.value === other.value
   }
 
+  /**
+   * Returns a string representation of the Name
+   * @returns String representation of the Name
+   */
   public toString(): string {
     return this.value
   }
--- src/flashcards/ports/repositories/note-type-repository.ts
+++ src/flashcards/ports/repositories/note-type-repository.ts
@@ -1,7 +1,20 @@
 import { Identity } from '@src/core/models/identity'
 import { NoteType } from '@src/flashcards/models/note-type'
 
+/**
+ * Note type repository.
+ */
 export interface INoteTypeRepository {
+  /**
+   * Saves the note type
+   * @param noteType Note type to add
+   */
   save(noteType: NoteType): void
+
+  /**
+   * Gets the note type by identity
+   * @param identity Identity of the note type
+   * @returns Note type or undefined if not found
+   */
   find(identity: Identity): NoteType | undefined
 }
